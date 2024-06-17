cd4958bc6a385f72e4ee7b54db9214be93f74285
IamPierrot <phatnguyentan836@gmail.com>
2024-06-17 20:17:23 +0700

    🌳 tree:  reorder food models

--- Models/Food.cs
+++ Models/Food.cs
@@ -38,5 +38,30 @@ public class FoodPost : IFoodBase
     public string ImageUrl { get; set; } = null!;
     public string Instruction { get; set; } = null!;
     public List<Ingredient>? Ingredients { get; set; } = null!;
-    public Nutrition Nutrition { get; set; }= null!;
+    public Nutrition Nutrition { get; set; } = null!;
+}
+
+public class Ingredient
+{
+    [BsonElement("name")]
+    public string Name { get; set; } = null!;
+
+    [BsonElement("quantity")]
+    public string Quantity { get; set; } = null!;
+}
+
+
+public class Nutrition
+{
+    [BsonElement("calories")]
+    public int Calories { get; set; }
+
+    [BsonElement("protein")]
+    public int Protein { get; set; }
+
+    [BsonElement("fat")]
+    public int Fat { get; set; }
+
+    [BsonElement("carbs")]
+    public int Carbs { get; set; }
 }
